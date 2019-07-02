ba7684eacb5d759f5710398eec2fbc6878c560bc
narinder19 <ngupta@columbiacountyga.gov>
2019-07-02 12:24:18 -0400

    Removed code

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
-import math
-import os
-import sys
+
 import requests
 
-print(sys.version)
-print(sys.executable)
 
 
-def greet(who_to_greet):
-    print("Hello World")
+
+
 
 
 r = requests.get("https://google.com")
 print(r.status_code)
+print(r.ok)
 
